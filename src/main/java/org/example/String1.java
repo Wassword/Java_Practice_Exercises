7feb00bc987acdf8810639b51e97435aef27eaa4
Wassword <ahmedsaba271@gmail.com>
2024-08-02 18:01:51 -0400

    final commit

---
+++ src/main/java/org/example/String1.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class String1 {
+    public String helloName(String name) {
+        // Return the greeting message with the provided name
+        return "Hello " + name + "!";
+    }
+}
+
+
